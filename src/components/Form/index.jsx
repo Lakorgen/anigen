a64b2213256ed2244f3678c044c386d778fff710
vlad <oskin0425@gmail.com>
2024-12-27 20:03:44 +0400

    Added validation login and register form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,53 +1,86 @@
 import React from "react";
 import styles from "./Form.module.scss";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-const Form = ({ title, handleClick, handleGoogleLogin }) => {
-  const [email, setEmail] = React.useState("");
-  const [pass, setPass] = React.useState("");
+const CustomForm = ({ title, handleClick, handleGoogleLogin }) => {
+  // const [email, setEmail] = React.useState("");
+  // const [pass, setPass] = React.useState("");
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Неправильный email адрес")
+        .required("Поле не может быть пустым"),
+      password: Yup.string()
+        .min(8, "Пароль должен содержать 8 символов")
+        .required("Поле не может быть пустым"),
+    }),
+    onSubmit: (values) => handleClick(values.email, values.password),
+    // onSubmit: () => handleClick(email, pass),
+  });
 
   return (
     <>
-      <div className={styles.inputContainer}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          id="username"
-          className={styles.input}
-          placeholder=" "
-        />
-        <label htmlFor="username" className={styles.label}>
-          Логин
-        </label>
-      </div>
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
+        <div className={styles.inputContainer}>
+          <input
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            id="email"
+            name="email"
+            className={styles.input}
+            placeholder=" "
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
+        </div>
+        {formik.errors.email && formik.touched.email && (
+          <div className={styles.error}>{formik.errors.email}</div>
+        )}
 
-      <div className={styles.inputContainer}>
-        <input
-          type="password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-          id="password"
-          className={styles.input}
-          placeholder=" "
-        />
-        <label htmlFor="password" className={styles.label}>
-          Пароль
-        </label>
-      </div>
-      <button
-        onClick={() => handleClick(email, pass)}
-        className={styles.button}
-      >
-        {title}
-      </button>
-      <button
-        onClick={handleGoogleLogin}
-        className={styles.button}
-      >
-        Вход через Google
-      </button>
+        <div className={styles.inputContainer}>
+          <input
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            id="password"
+            name="password"
+            className={styles.input}
+            placeholder=" "
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="password" className={styles.label}>
+            Пароль
+          </label>
+        </div>
+        {formik.errors.password && formik.touched.password && (
+          <div className={styles.error}>{formik.errors.password}</div>
+        )}
+        <button
+          type="submit"
+          disabled={!formik.isValid || !formik.dirty}
+          className={styles.button}
+        >
+          {title}
+        </button>
+        <button
+          type="button"
+          onClick={handleGoogleLogin}
+          className={styles.button}
+        >
+          Вход через Google
+        </button>
+      </form>
     </>
   );
 };
 
-export default Form;
+export default CustomForm;
